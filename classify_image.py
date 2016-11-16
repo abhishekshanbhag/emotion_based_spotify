fbb06ee1d9f32262dfa6e472eefe8ef6dae83e57
andrewLevy <levya@bu.edu>
2016-11-16 17:41:57 -0500

    Updated Classify Script (Requires AWS key file)

--- classify_image.py
+++ classify_image.py
@@ -1,16 +1,32 @@
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.models import model_from_json
 import numpy
-import os
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.models import model_from_json
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
 from boto.s3.connection import S3Connection
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 
 # Download image from Amazon s3 bucket
-AWS_KEY = 'AKIAII34U7AV4AUDSULA'
-AWS_SECRET = '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'
+import sys
+with open('rootkey_2.csv',"r") as infile:
+    text = infile.readlines()
+    infile.close()
+
+AWS_KEY = text[0].strip()
+AWS_SECRET = text[1].strip()
+
+AWS_KEY = AWS_KEY[15:]
+AWS_SECRET = AWS_SECRET[13:]
+
 aws_connection = S3Connection(AWS_KEY, AWS_SECRET)
 bucket = aws_connection.get_bucket('ec601imagebucket')
 for file_key in bucket.list():
@@ -20,6 +36,12 @@ file_key.get_contents_to_filename('test_images.csv')
 
 testset = numpy.loadtxt("test_images.csv", delimiter=",")
 X_test = testset[:,0:2304]
+y_test = testset[:,2304]
+
+X_test = X_test.reshape(X_test.shape[0], 1, 48, 48).astype('float32')
+X_test = X_test / 255
+
+y_test = np_utils.to_categorical(y_test)
 # Y_test = testset[:,2304]
 
 
@@ -33,11 +55,11 @@ loaded_model.load_weights("model.h5")
 print("Loaded model from disk")
 
 # evaluate loaded model on test data
-loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-label_predict = loaded_model.predict(X_test)
+loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# label_predict = loaded_model.predict(X_test)
 # image_num = 2;
-print(label_predict[3])
+# print(label_predict[3])
 # rounded_predict = [round(label_predict) for x in label_predict]
-print(round(label_predict[3]))
-# score = loaded_model.evaluate(X_test, Y_test, verbose=0)
-# print "%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100)
+# print(round(label_predict[3]))
+score = loaded_model.evaluate(X_test, y_test, verbose=0)
+print "%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100)
