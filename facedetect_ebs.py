006293d0f620f025f63d6f9bcde200b8aab0bad7
Abhishek Shanbhag <anshanbhag93@gmail.com>
2016-12-07 17:07:12 -0500

    Face Detect Selects largest face and resizes to 48x48

--- facedetect_ebs.py
+++ facedetect_ebs.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python
 import cv2
-
+from os import listdir
 
 def detect(img, cascade):
+    rects = []
     rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv2.CASCADE_SCALE_IMAGE)
     if len(rects) == 0:
-        rects = []
-    rects[:,2:] += rects[:,:2]
+        rects = list([])
+    else:
+        rects[:,2:] += rects[:,:2]
     return rects
-
 
 cascade_fn = "face_detection_files/haarcascades/haarcascade_frontalface_alt.xml"
 cascade = cv2.CascadeClassifier(cascade_fn)
-img = cv2.imread('face_detection_files/20110204.jpg')
+
+img = cv2.imread('')
+#cv2.imshow('image', img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.equalizeHist(gray)
+#cv2.imshow('gray', gray)
+#gray = cv2.equalizeHist(gray)
 rects = detect(gray, cascade)
 if(len(rects)):
+    i = 0
+    biggest_sz = 0
+    biggest_index = 0
     for x1, y1, x2, y2 in rects:
-        roi = gray[y1:y2, x1:x2]
+        temp_face = gray[y1:y2, x1:x2]
+        sz1 = (y2-y1)*(x2-x1)
+        if sz1 > biggest_sz:
+            biggest_sz = sz1
+            biggest_index = i
+        i = i+1
+    x1, y1, x2, y2 = rects[biggest_index]
+    roi = gray[y1:y2, x1:x2]
 else:
     roi = gray
+    print('face not detected. Please click from another angle of lighting.')
+roi = cv2.equalizeHist(roi)
+roi = cv2.resize(roi, (48, 48))
+#cv2.imshow('facedetect', roi)
+#cv2.waitKey(0)
 
-cv2.imshow('facedetect', roi)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
+
